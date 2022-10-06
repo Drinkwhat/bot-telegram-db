cef7e69b6db410fe5fc8e39eb7a1bb7b5718fd92
Luca Bevilacqua <luca.bevilacquaxxx5@gmail.com>
2022-10-06 16:08:23 +0200

    add check identity

--- src/app.ts
+++ src/app.ts
@@ -14,71 +14,85 @@ bot.command("start", async(ctx) => {
   void ctx.reply("I'm online")
 })
 
-bot.command("echo", async(ctx) => {
-  const message = ctx.message.text.split(" ")
-  message.shift()
-  void ctx.reply(message.join(" "))
-})
-
 bot.command("add", async(ctx) => {
-  const message = ctx.message.text.split(" ")
-  message.shift()
-  try {
-    if (message[1]) {
-      void update(message[0].toLowerCase(), Math.abs(Number(message[1])))
-    } else {
-      void update(message[0].toLowerCase(), 1)
+  if (String(ctx.message.chat.id) !== process.env.CHAT_ID) {
+    return
+  } else {
+    const message = ctx.message.text.split(" ")
+    message.shift()
+    try {
+      if (message[1]) {
+        void update(message[0].toLowerCase(), Math.abs(Number(message[1])))
+      } else {
+        void update(message[0].toLowerCase(), 1)
+      }
+    } catch (err) {
+      void ctx.reply(`error: ${err}`)
     }
-  } catch (err) {
-    void ctx.reply(`error: ${err}`)
   }
 })
 
 bot.command("delete", async(ctx) => {
-  const message = ctx.message.text.split(" ")
-  message.shift()
-  try {
-    if (message[1]) {
-      void update(message[0].toLowerCase(), -1 * Math.abs(Number(message[1])))
-    } else {
-      void update(message[0].toLowerCase(), -1)
+  if (String(ctx.message.chat.id) !== process.env.CHAT_ID) {
+    return
+  } else {
+    const message = ctx.message.text.split(" ")
+    message.shift()
+    try {
+      if (message[1]) {
+        void update(message[0].toLowerCase(), -1 * Math.abs(Number(message[1])))
+      } else {
+        void update(message[0].toLowerCase(), -1)
+      }
+    } catch (err) {
+      void ctx.reply(`error: ${err}`)
     }
-  } catch (err) {
-    void ctx.reply(`error: ${err}`)
   }
 })
 
 bot.command("remove", async(ctx) => {
-  const message = ctx.message.text.split(" ")
-  message.shift()
-  try {
-    void remove(message[0].toLowerCase())
-  } catch (err) {
-    void ctx.reply(`error: ${err}`)
+  if (String(ctx.message.chat.id) !== process.env.CHAT_ID) {
+    return
+  } else {
+    const message = ctx.message.text.split(" ")
+    message.shift()
+    try {
+      void remove(message[0].toLowerCase())
+    } catch (err) {
+      void ctx.reply(`error: ${err}`)
+    }
   }
 })
 
 bot.command("inventory", async(ctx) => {
-  const message = ctx.message.text.split(" ")
-  message.shift()
-  try {
-    const inventory = await selectAll()
-    for (let i = 0; i < inventory.length; i++) {
-      const data = String(Object.values(inventory[i])[0]).slice(1, -1).split(",").join(": ")
-      void ctx.reply(data)
+  if (String(ctx.message.chat.id) !== process.env.CHAT_ID) {
+    return
+  } else {
+    const message = ctx.message.text.split(" ")
+    message.shift()
+    try {
+      const inventory = await selectAll()
+      for (let i = 0; i < inventory.length; i++) {
+        const data = String(Object.values(inventory[i])[0]).slice(1, -1).split(",").join(": ")
+        void ctx.reply(data)
+      }
+    } catch (err) {
+      void ctx.reply(`error: ${err}`)
     }
-  } catch (err) {
-    void ctx.reply(`error: ${err}`)
   }
 })
 
 bot.command("recount", async(ctx) => {
-  const message = ctx.message.text.split(" ")
-  message.shift()
-  try {
-    void recount(message[0].toLowerCase(), Math.abs(Number(message[1])))
-  } catch (err) {
-    void ctx.reply(`error: ${err}`)
+  if (String(ctx.message.chat.id) !== process.env.CHAT_ID) {
+    return
+  } else {
+    const message = ctx.message.text.split(" ")
+    message.shift()
+    try {
+      void recount(message[0].toLowerCase(), Math.abs(Number(message[1])))
+    } catch (err) {
+      void ctx.reply(`error: ${err}`)
+    }
   }
 })
 
